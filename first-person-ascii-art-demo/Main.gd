6fe6ec7568986d2cd03cf0b2ab3717675097a245
odd-stick <rdcoll@hotmail.com>
2021-01-28 13:41:27 -0500

    Fixed main scenes

--- first-person-ascii-art-demo/Main.gd
+++ first-person-ascii-art-demo/Main.gd
@@ -8,12 +8,14 @@ onready var start_handle : Position3D = $"Viewport/Level/Start"
 
 func _ready():
 	
+	pass
+	
 	# just a message box to remind everyone how to tune the fidelity
 	# OS.alert('You can use the PAGE_UP and PAGE_DOWN keys to control the fidelity (i.e., colour fidelity and size of character palette) of the ASCII art representation of the first-person view.', 'Important Note')
 	
 	# inside the level there is a Position3D named "Start" that can be used as
 	# the entry point for the player into the scene
-	player_handle.transform = start_handle.transform
+	# player_handle.transform = start_handle.transform
 
 
 func _input(event):
