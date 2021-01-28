b8a32c4444f878dd24fa71dc5a6878206e929184
Matt Diener <mattndiener@gmail.com>
2021-01-27 22:18:22 -0500

    a

--- first-person-ascii-art-demo/Main.gd
+++ first-person-ascii-art-demo/Main.gd
@@ -2,10 +2,9 @@ extends Node2D
 
 
 onready var player_handle : KinematicBody = $"Viewport/Spatial/Player"
+onready var viewport : Viewport = $"Viewport"
 
-
-func _ready():
-	
+func _ready():	
 	# just a message box to remind everyone how to tune the fidelity
 	OS.alert('You can use the PAGE_UP and PAGE_DOWN keys to control the fidelity (i.e., colour fidelity and size of character palette) of the ASCII art representation of the first-person view.', 'Important Note')
 
@@ -17,17 +16,6 @@ func _input(event):
 	# actually needed in order to get the mouse look to work)
 	if event is InputEventMouseMotion:
 		player_handle.mouse_movement = event.relative
-
-	# Give debug views with f keys
-	if Input.is_action_pressed("f1"):
-		$FirstPerson.visible = true
-		$AsciiArt.visible = false
-		$Viewport.set_debug_draw(0)
-	elif Input.is_action_pressed("f2"):
-		$FirstPerson.visible = false
-		$AsciiArt.visible = true
-		$Viewport.set_debug_draw(0)
-	elif Input.is_action_pressed("f3"):
-		$FirstPerson.visible = false
-		$AsciiArt.visible = true
-		$Viewport.set_debug_draw(1)
+		
+	if event is InputEventKey:
+		player_handle.handle_key_press(event)
