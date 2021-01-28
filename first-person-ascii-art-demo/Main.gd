e100d0514d16185e7fdba14d38aca7c78d096e95
Forest Anderson <forestkzanderson@gmail.com>
2021-01-28 13:03:00 -0500

    Merged Rob's code

--- first-person-ascii-art-demo/Main.gd
+++ first-person-ascii-art-demo/Main.gd
@@ -3,14 +3,25 @@ extends Node2D
 
 onready var player_handle : KinematicBody = $"Viewport/3DWorld/Player"
 onready var viewport : Viewport = $"Viewport"
+onready var start_handle : Position3D = $"Viewport/Level/Start"
 
-func _ready():	
+
+func _ready():
+	
 	# just a message box to remind everyone how to tune the fidelity
-	OS.alert('You can use the PAGE_UP and PAGE_DOWN keys to control the fidelity (i.e., colour fidelity and size of character palette) of the ASCII art representation of the first-person view.', 'Important Note')
+	# OS.alert('You can use the PAGE_UP and PAGE_DOWN keys to control the fidelity (i.e., colour fidelity and size of character palette) of the ASCII art representation of the first-person view.', 'Important Note')
+	
+	# inside the level there is a Position3D named "Start" that can be used as
+	# the entry point for the player into the scene
+	player_handle.transform = start_handle.transform
 
-	
+
 func _input(event):
-	
+
+	# for debugging purposes, since the mouse is locked, escape can be used to quit
+	if Input.is_action_pressed("debug_exit"):
+		get_tree().quit()
+
 	# it is necessary to pass the mouse movement from the Main node (where
 	# it gets captured), into the Player node in the Viewport (where it is
 	# actually needed in order to get the mouse look to work)
---
+++ first-person-ascii-art-demo/Terminal.gd
@@ -0,0 +1,11 @@
+extends Node
+
+# this is a work in progress, but this node is going to control the traditional
+# interactive fiction interface
+var screen_buffer_data = []
+var screen_buffer_wide : int = 60
+var screen_buffer_high : int = 45
+var current_line_index : int = 0
+
+func _ready():
+	pass
