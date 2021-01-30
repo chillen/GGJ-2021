3839957e69aec342a0eb1be17177594abebefd66
ChristopherBlackman <mr.christopher.blakman@gmail.com>
2021-01-30 14:44:49 -0500

    added torch pickup, item equips, item de-equpts

--- first-person-ascii-art-demo/Interactables/Torch.gd
+++ first-person-ascii-art-demo/Interactables/Torch.gd
@@ -1,5 +1,25 @@
 extends Spatial
 
+export var on = false
+
+func _ready():
+	if on:
+		$TorchLight.show()
+	else:
+		$TorchLight.hide()
 
 func _on_Interactable_interacted(interaction_string,interaction_caller):
-	print("Pickup torch")
+	if interaction_string == "take":
+		interaction_caller.equip_item(self)
+		self.rotation_degrees = Vector3(0,0, 0)
+		self.translation = Vector3(0,0,0)
+	if interaction_string == "drop":
+		interaction_caller.de_equip_item(self)
+	if interaction_string == "use":
+		if on:
+			$TorchLight.hide()
+		else:
+			$TorchLight.show()
+		on = !on
+	
+	
