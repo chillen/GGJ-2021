9ad8572a733d58c537b4714278b4092c250a6b97
ChristopherBlackman <mr.christopher.blakman@gmail.com>
2021-01-29 21:44:15 -0500

    Door Update
    - added interaction

--- first-person-ascii-art-demo/DoorTemplate.gd
+++ first-person-ascii-art-demo/DoorTemplate.gd
@@ -1,6 +1,8 @@
 extends Spatial
 
 export var animation_open = "Hinge_Open"
+export var is_open = false
+export var locked = false
 
 signal open
 signal closed
@@ -9,6 +11,10 @@ signal closed
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if is_open:
+		 $AnimationPlayer.play(animation_open)
+	else:
+		$AnimationPlayer.play_backwards(animation_open)
 	pass # Replace with function body.
 
 
@@ -16,13 +22,34 @@ func _ready():
 #func _process(delta):
 #	pass
 #	pass
+func interaction():
+	if locked:
+		# print something to terminal
+		# play sound that might sugest locked
+		return
+	if is_open:
+		close()
+	else:
+		open()
+	
 
 func open():
+	if is_open:
+		return
+	if $AnimationPlayer.is_playing():
+		return
+		
 	$AnimationPlayer.play(animation_open)
 	emit_signal("open")
 	pass
 	
+	
 func close():
+	if not is_open:
+		return
+	if $AnimationPlayer.is_playing():
+		return
+		
 	$AnimationPlayer.play_backwards(animation_open)
 	emit_signal("closed")
 	pass
